efca90fe2fea5828b35bb0de3259cd7b57f3e35c
Earl Francis <earlfrancis0311@gmail.com>
2021-07-09 17:05:54 +0100

    set up message form container

---
+++ src/containers/message_form.jsx
@@ -0,0 +1,34 @@
+  
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { createMessage } from '../actions/index';
+
+class MessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  componentDidMount() {
+    this.messageBox.focus();
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.createMessage(this.props.selectedChannel, this.props.currentUser, this.state.value);
+    this.setState({ value: '' });
+  }
+
+  render() {
+    return (
+      <form>
+        
+      </form>
+    )
+  }
+}
